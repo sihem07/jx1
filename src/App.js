9d5ec43c5ac78f82446c2d4e51deba7fad2b7575
sihem <ramouch1920@gmail.com>
2021-03-26 11:57:57 +0100

    chjx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import './rd.css';
 
+import imagesrc from './logo.svg'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{border:'solid 1px black', maxWidth:'100vw'}}>
+      <h1 className="title red">Your name here</h1>
+      <br/ >
+<img src={imagesrc} />
+<br/>
+<img src="logo512.png" />
+</div>
+<video width="320" height="240" controls>
+
+ <source src="myVideo.mp4" type="video/mp4" />
+
+</video>
+
     </div>
-  );
+
+ );
 }
 
 export default App;
